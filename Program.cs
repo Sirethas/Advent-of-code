68fe715eb9ec43260a1f7661c6d31a0ff689b0f8
Jakub Jasan <jakub.jasan1@gmail.com>
2024-12-06 12:44:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Advent;
+
+DayOne.Solve();
+Console.WriteLine("-----------------------------");
+DayTwo.Solve();
+Console.WriteLine("-----------------------------");
+DayThree.Solve();
+Console.WriteLine("-----------------------------");
+DayFour.Solve();
